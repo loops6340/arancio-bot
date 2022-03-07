e620579b82681a09a553fa71b8ed5666f6a98afc
Emiliar2002 <earancio616@gmail.com>
2022-03-07 06:41:02 -0300

    MATAR CASI IMPLEMENTADO

--- src/index.js
+++ src/index.js
@@ -10,8 +10,10 @@ require("./libros.js")
 const imagenesDoxxeo = ["https://cdn.discordapp.com/attachments/924175007494569984/948444002775105556/gosling.jpg", "https://media.discordapp.net/attachments/684197130801512469/873448820548173886/FB_IMG_1625793045511.png", "https://cdn.discordapp.com/attachments/924175007494569984/948443507390050314/thanos.0.jpeg", "https://cdn.discordapp.com/attachments/924175007494569984/948443536880205924/Troll_face.png", "https://media.discordapp.net/attachments/684197130801512469/873450402463498240/trejo11.png", "https://cdn.discordapp.com/attachments/924175007494569984/948443366927007824/big_knuckknuck.png", "https://cdn.discordapp.com/attachments/924175007494569984/948443266087522355/24448_Pulp-Fiction-reestreno-espana-cines.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948443242972729344/poison.png","https://cdn.discordapp.com/attachments/924175007494569984/948443123430883398/1522543437757.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948443024923447326/Joker-790658206-large.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948442948670996480/goodfellas.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948442901074047047/Fullenght.png", "https://cdn.discordapp.com/attachments/924175007494569984/948442855867834368/frost.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948442827363323914/FLl8NnTaMAI2NYf.jpg", "https://media.discordapp.net/attachments/552168964076208143/718961911890444298/IMG_20200605_011506.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948442703920762950/e23c2f2b418bbab27b239d67ae6ddc31.png", "http://pm1.narvii.com/6207/d114704ab345a911e3e8a59c7e11840c27d16cdd_00.jpg", "https://lastfm.freetls.fastly.net/i/u/500x500/01cb72e57c87fa4dfecbec94cf3c5c82.jpg", "https://media.revistagq.com/photos/5ca6057e071fbf7b7b71aaa8/16:9/w_1280,c_limit/tyler_durden_2602.jpg", "https://assets-global.website-files.com/605a3cacba514a77ca2e6ab5/60c21dae537b04cf56271def_drive-2.jpg", "https://media.vozpopuli.com/2021/02/caracterizacion-Patrick-Bateman-Mary-Harron_896920346_1372092_1020x574.jpg", "https://i.ytimg.com/vi/fiBLgEx6svA/maxresdefault.jpg", "https://gcdn.lanetaneta.com/wp-content/uploads/2022/01/1641108913_La-estrella-de-Impractical-Jokers-Joe-Gatto-anuncia-su-salida.jpg", "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2015/04/95797-trollface.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948441609786253322/CR_967926_f20505bf0d4242c0b587873e6ef6f768_tecnologia_thumb_fb.png", "https://cdn.discordapp.com/attachments/924175007494569984/948441598872682546/1639109873075.jpg", "https://media.discordapp.net/attachments/924175007494569984/948441784567103498/unknown.png", "https://media.discordapp.net/attachments/684115741133242369/752344986594574336/sk997k5zlpd11.png", "https://media.discordapp.net/attachments/684115741133242369/747307911730036776/1598235013801.png", "https://cdn.discordapp.com/attachments/924175007494569984/948442302781751326/1645135124086.png", "https://cdni.rt.com/actualidad/public_images/2016.08/article/57bee01ec461888a0d8b4567.jpg", "https://cdn.discordapp.com/attachments/924175007494569984/948442457308299304/aso.png", "https://cdn.discordapp.com/attachments/924175007494569984/948442514665381890/breaking_bad_vince_gilligan_amc.webp"]
 const danbooruRandom = "https://danbooru.donmai.us/posts/random.json?"
 const danbooruSafe = "+rating:safe"
+
+
 const literalmenteAso = [`${danbooruRandom}tags=m200_(girls'_frontline)${danbooruSafe}`, `${danbooruRandom}tags=hatoba_tsugu${danbooruSafe}`, `${danbooruRandom}tags=nagato_yuki${danbooruSafe}`, `${danbooruRandom}tags=okita_souji_(fate)${danbooruSafe}`, `${danbooruRandom}tags=hisui_(tsukihime)${danbooruSafe}`, `${danbooruRandom}tags=oda_nobunaga_(fate)${danbooruSafe}`, `${danbooruRandom}tags=nazrin${danbooruSafe}`, `${danbooruRandom}tags=bernadetta_von_varley${danbooruSafe}`, `${danbooruRandom}tags=ryougi_shiki${danbooruSafe}`]
-const literalmenteZerhin = [`${danbooruRandom}tags=anchovy_(girls_und_panzer)${danbooruSafe}`, `${danbooruRandom}tags=satanichia_kurumizawa_mcdowell${danbooruSafe}`, `${danbooruRandom}tags=m14_(girls'_frontline)${danbooruSafe}`, `${danbooruRandom}tags=s.a.t.8_(girls'_frontline)${danbooruSafe}`, `${danbooruRandom}tags=super-shorty_(girls'_frontline)${danbooruSafe}`, `${danbooruRandom}tags=usada_hikaru${danbooruSafe}`, `${danbooruRandom}tags=shiki_eiki${danbooruSafe}`, `${danbooruRandom}tags=el_condor_pasa_(umamusume)${danbooruSafe}`]
+const literalmenteZerhin = [`${danbooruRandom}tags=anchovy_(girls_und_panzer)${danbooruSafe}`, `${danbooruRandom}tags=satanichia_kurumizawa_mcdowell${danbooruSafe}`, `${danbooruRandom}tags=m14_(girls'_frontline)${danbooruSafe}`, `${danbooruRandom}tags=s.a.t.8_(girls'_frontline)${danbooruSafe}`, `${danbooruRandom}tags=super-shorty_(girls'_frontline)${danbooruSafe}`, `${danbooruRandom}tags=usada_hikaru${danbooruSafe}`, `${danbooruRandom}tags=shiki_eiki${danbooruSafe}`, `${danbooruRandom}tags=el_condor_pasa_(umamusume)${danbooruSafe}`, `${danbooruRandom}tags=tressa_(octopath_traveler)${danbooruSafe}`]
 const literalmenteHola = [`${danbooruRandom}tags=kuroki_tomoko${danbooruSafe}`, `${danbooruRandom}tags=kaname_madoka${danbooruSafe}`, `${danbooruRandom}tags=yumemi_riamu${danbooruSafe}`, `${danbooruRandom}tags=ohara_mari${danbooruSafe}`, `${danbooruRandom}tags=majima_gorou${danbooruSafe}` ]
 const literalmenteVenom = [`${danbooruRandom}tags=aisaka_taiga${danbooruSafe}`]
 const literalmenteMeiggs = [`${danbooruRandom}tags=nekomata_okayu${danbooruSafe}`]
@@ -19,6 +21,9 @@ const literalmenteGor = [`${danbooruRandom}tags=louise_francoise_le_blanc_de_la_
 const literalmenteLoops = [`${danbooruRandom}tags=kirigiri_kyouko${danbooruSafe}`, `${danbooruRandom}tags=nanami_chiaki${danbooruSafe}`, `${danbooruRandom}tags=lelouch${danbooruSafe}`]
 const literalmenteLac = [`${danbooruRandom}tags=al_azif${danbooruSafe}`, `${danbooruRandom}tags=noelle_(genshin_impact)${danbooruSafe}`, `https://g.tenor.com/v1/random?q=super%20robot%20wars&key=RL591LH7DHMW&limit=1`]
 const literalmenteNum = [`${danbooruRandom}tags=akemi_homura${danbooruSafe}`, `${danbooruRandom}tags=yoshikawa_chinatsu${danbooruSafe}`, `${danbooruRandom}tags=senko_(sewayaki_kitsune_no_senko-san)${danbooruSafe}`, `${danbooruRandom}tags=chieru_(princess_connect!)${danbooruSafe}`, `${danbooruRandom}tags=hoshizora_miyuki${danbooruSafe}`, `${danbooruRandom}tags=rosehip_(girls_und_panzer)${danbooruSafe}`, `${danbooruRandom}tags=dante_(devil_may_cry)${danbooruSafe}`, `${danbooruRandom}tags=char_aznable${danbooruSafe}`, `${danbooruRandom}tags=uchiha_itachi${danbooruSafe}`, `${danbooruRandom}tags=yoshikawa_chinatsu${danbooruSafe}`, `${danbooruRandom}tags=nakano_azusa${danbooruSafe}`, `${danbooruRandom}tags=godot_(ace_attorney)${danbooruSafe}`, `https://g.tenor.com/v1/random?q=mobile%20suit%20gundam&key=RL591LH7DHMW&limit=1`, `${danbooruRandom}tags=ryuk${danbooruSafe}`]
+
+const insultos = ["ESTE ES TU FIN", "SI HUBIERA CP DE UN VIDEO TUYO LA VERDAD ME MASTURBARIA VIENDOLO", "TE VOY A MUTILAR", "<<<<<<<<<<<<<<<<<<<<<<< ESTE USUARIO PUBLICA GORE DECAPITACIONES Y PEDOFILIA", "PUTO IMBECIL XD TIO DE VERDAD OJALA TE MUERAS (EN LA VIDA REAL)", "GILIPOLLAS", "NEGRO DE MIERDA", "ES UN BOLUDO UN FORRO UN SORETE UN MALNACIDO UN SOPENCO UN PELOTUDO UN IDIOTA UN IMBECIL UN GORDO UN CHUPAPIJA UN RECHUPAVERGA UN"]
+
 const client = new DiscordJS.Client({
   intents: [
     "GUILDS",
@@ -46,7 +51,11 @@ client.on('ready', () => {
 })
 
 
-
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 
 
@@ -188,7 +197,7 @@ GRAN PAIS XD`)
             else{
               const imagenRandom = await request.data.file_url
               message.channel.send(imagenRandom)
-           }       
+           }
         }
         else if(args === "num"){
           const indexRandom = literalmenteNum[Math.floor(Math.random()*literalmenteNum.length)]
@@ -223,16 +232,43 @@ GRAN PAIS XD`)
         else if (command === "insultar"){
           if(message.mentions.members.size > 0){
             const desgraciado = args
+            const unomismo = `<@!${message.author.id}>`
             if(desgraciado === bot.tag){
               message.channel.send("no molestes");
             }
+            else if(desgraciado === unomismo){
+              message.channel.send(`no tio, no te tienes que hacer esto de verdad eres la polla con beceboya xD`)
+            }
             else{
-            message.channel.send(`${desgraciado} HIJO DE PUTA TE VOY A MUTILAR`);
+              message.channel.send(`${desgraciado} ${insultos[Math.floor(Math.random()*insultos.length)]}`);
           }}
           else{
-            message.channel.send(`${message.author} PUTO IDIOTA DE VERDAD XD.`);
+            message.channel.send(`${message.author} <<<<<<<<<<<<<<< PUTO IMBECIL XD`);
           }
         }
+
+        else if (command === "MATAR"){
+          if(message.mentions.members.size > 0){
+          const desgraciado = args
+          const unomismo = `<@!${message.author.id}>`
+          const causaDeMuerte = "??????????NOSE"
+          if(desgraciado === bot.tag){
+            message.channel.send(`VALE TIO COMO TU DIGAS.`);
+            await sleep(40000);
+            message.channel.send(`${message.author} HA SIDO DESPEDAZADO BRUTALMENTE...`);
+          }
+          else if(desgraciado === unomismo){
+            message.channel.send(`no tio, no te tienes que hacer esto de verdad eres la polla con beceboya xD`)
+          }
+          else{
+            message.channel.send(`VALE TIO COMO TU DIGAS.`);
+            await sleep(40000);
+            message.channel.send(`${desgraciado} HA SIDO ${causaDeMuerte}`);
+        }}
+        else{
+          message.channel.send(`${message.author} <<<<<<<<<<<<<<< PUTO IMBECIL XD`);
+        }
+      }
         else if (command === "invitarmierda"){
           message.channel.send("LINK DE INVITACION CARAJO https://discord.com/api/oauth2/authorize?client_id=947306117527142410&permissions=8&scope=bot")
         }
@@ -246,7 +282,8 @@ quote: FRASE RANDOM DE UN LIBRO DE UNA METICULOSA SELECCION REALIZADA POR EL INT
 dolar: MUESTRA EL PRECIO DEL DOLAR HOY EN ARGENTINA.
 literalmente (miembro): LITERALMENTE.
 tweet (tweet): SE EXPLICA SOLO.
-insultar (usuario): LO HUMILLO.
+insultar (@usuario): LO HUMILLO.
+MATAR (CAUSA DE MUERTE): LO ANOTO EN MI LIBRETA DE MUERTE POR GILIPOLLAS.
 invitarmierda: INVITACION MIERDA`)
         }
         else {
